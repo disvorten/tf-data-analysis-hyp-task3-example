fb754749fb6305b3a77aa7d70c24e89b345ca43b
disvorten <124072944+disvorten@users.noreply.github.com>
2023-04-08 22:37:22 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
+
+chat_id = 1109095907  # Ваш chat ID, не меняйте название переменной
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x, y) -> bool:
+    alpha = 0.06
+    a, b = ttest_ind(x, y, alternative='greater')
+    if b <= alpha:
+        return True
+    else:
+        return False
