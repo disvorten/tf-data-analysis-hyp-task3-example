d821f437efc06ac895057103d78d85b428c357b9
disvorten <124072944+disvorten@users.noreply.github.com>
2023-04-09 17:12:57 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_ind
+from scipy.stats import mannwhitneyu
 
 chat_id = 1109095907  # Ваш chat ID, не меняйте название переменной
 
 
 def solution(x, y) -> bool:
     alpha = 0.06
-    res = ttest_ind(x, y, alternative='greater')
+    res = mannwhitneyu(x, y, alternative='greater', method='asymptotic')
     if res.pvalue <= alpha:
         return True
     else:
